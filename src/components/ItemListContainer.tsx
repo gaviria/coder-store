317f48bd6bcf8fb1e6142a51e6a6fd3ec481f7af
Francisco <gaviria.reyes@gmail.com>
2023-01-28 13:40:44 -0500

    Creando ItemListContainer

---
+++ src/components/ItemListContainer.tsx
@@ -0,0 +1,9 @@
+type ItemListProps = {
+	greeting: string;
+};
+
+const ItemListContainer = ({ greeting }: ItemListProps) => {
+	return <div>ItemListContainer</div>;
+};
+
+export default ItemListContainer;
