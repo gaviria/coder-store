f8d4bc450607b13ac3d1b7b292bdd6e5babb89ce
Francisco <gaviria.reyes@gmail.com>
2023-02-18 00:36:27 -0500

    adding types

---
+++ src/types/ItemList.ts
@@ -0,0 +1,17 @@
+export type itemListType = {
+	id: number;
+	title: string;
+	description: string;
+	price: number;
+	pictureUrl: string;
+	stock: number;
+}[];
+
+export type itemType = {
+	id: number;
+	title: string;
+	description: string;
+	price: number;
+	pictureUrl: string;
+	stock: number;
+};
